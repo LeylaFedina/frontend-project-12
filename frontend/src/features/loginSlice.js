a18cb22a9e1036e6c7ad078497870de9310d74f2
LeylaFedina <Leyla_Ismailova_B@mail.ru>
2025-02-15 14:54:27 +0400

    log slice upd

--- frontend/src/features/loginSlice.js
+++ frontend/src/features/loginSlice.js
@@ -55,7 +55,9 @@ const loginSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(loginUser.rejected, (state, { error }) => {
-        state.loginError = error.message;
+        if (error.message.includes("401")) {
+          state.loginError = "Неверные имя пользователя или пароль";
+        }
       })
       .addCase(loginUser.fulfilled, (state, { payload }) => {
         localStorage.setItem("userData", JSON.stringify(payload));
