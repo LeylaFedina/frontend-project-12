f9725dd3cb1d0060bfc38cdce3a5bd8c38ae7a71
LeylaFedina <Leyla_Ismailova_B@mail.ru>
2025-02-15 14:33:58 +0400

    step  8 login

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
+import { useTranslation } from 'react-i18next';
 
 import { loginUser } from '../../features/loginSlice';
 
 const Login = () => {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
   const loginError = useSelector((state) => state.login.loginError);
   const form = useFormik({
     initialValues: {
@@ -17,7 +19,7 @@ const Login = () => {
   });
   return (
     <form onSubmit={form.handleSubmit} className="col-12 col-md-6 mt-3 mt-md-0">
-      <h1 className="text-center mb-4">Войти</h1>
+      <h1 className="text-center mb-4">{t('loginPage.title')}</h1>
       <div className="form-floating mb-3">
         <input
           name="username"
@@ -29,7 +31,7 @@ const Login = () => {
           onChange={form.handleChange}
           value={form.values.username}
         />
-        <label htmlFor="username">Ваш ник</label>
+        <label htmlFor="username">{t('loginPage.form.login')}</label>
       </div>
       <div className="form-floating mb-4">
         <input
@@ -44,12 +46,12 @@ const Login = () => {
           value={form.values.password}
         />
         <label className="form-label" htmlFor="password">
-          Пароль
+        {t('loginPage.form.password')}
         </label>
         {loginError && <div className="invalid-feedback">{loginError}</div>}
       </div>
       <button type="submit" className="w-100 mb-5 btn btn-outline-primary">
-        Войти
+      {t('loginPage.form.loginBtn')}
       </button>
     </form>
   );
